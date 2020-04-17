0352abad56d10e990f42d910be4579ca4cdd088c
maddaj <j.maddalena2@gmail.com>
2020-04-17 11:22:51 +0200

    Exo 4 with explications

--- exo04.php
+++ exo04.php
@@ -5,15 +5,19 @@ function sum($arrayOfNumbers)
     $sum = 0;
 
     for ($i = 0; $i < count($arrayOfNumbers); $i++) {
+        // On boucle sur $array pour récupérer la valeur actuelle du tableau
         $currentElement = $arrayOfNumbers[$i];
 
         if (is_array($currentElement)) {
+            // Si la valeur contenue dans $currentElement est un tableau on lui applique la fonction Sum()
             $sum = $sum + sum($currentElement);
         } else {
+            // Si $currentElement est un entier et non un tableau on l'aditionne à la somme précédente
             $sum += $currentElement;
+            // La somme $sum vaudra la somme des précedents éléments plus la somme des élements du tableau $currentElement
         }
     }
-    return $sum;
+    return $sum; // On retourne la somme de tous les élements
 }
 
 $numbers = array(1, 2, 3, array(1, 2, 3, array(1, 2, 3)));
